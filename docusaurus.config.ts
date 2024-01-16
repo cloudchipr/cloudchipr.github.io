1eab37df3660fb5d3d231e95ef291b605c7bcfa1
vlad-cloudchipr <vlad@cloudchipr.com>
2024-01-16 04:11:54 +0400

    baseURL change

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/cc_icon_big.ico',
 
   // Set the production url of your site here
-  url: 'https://gh-pages.cloudchipr.dev',
+  url: 'https://cloduchipr.netlify.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
